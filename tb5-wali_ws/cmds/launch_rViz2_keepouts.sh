04adfe6ae5158aba84c4b823711ef8cd06229e99
slowrunner <slowrunner@users.noreply.github.com>
2026-04-11 22:39:00 -0400

    setup rviz2 to launch with keepouts and odometry

---
+++ tb5-wali_ws/cmds/launch_rViz2_keepouts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# have modified /opt/ros/jazzy/share/turtlebot4_viz/rviz/navigation.yaml 
+# for 2D only costmap display
+
+source ss.sh
+echo -e "\nLAUNCH rViz2 w/keepouts"
+# echo -e"ros2 launch turtlebot4_viz view_navigation.launch.py"
+# ros2 launch turtlebot4_viz view_navigation.launch.py
+ros2 launch rviz2/view_nav_w_keepouts.launch.py rviz2_config:=rviz2/tb5-wali_keepouts.rviz
--- tb5-wali_ws/cmds/launch_tb4_viz_view_nav.sh
+++ tb5-wali_ws/cmds/launch_tb4_viz_view_nav.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# have modified /opt/ros/jazzy/share/turtlebot4_viz/rviz/navigation.yaml 
+# for 2D only costmap display
+
 source ss.sh
 echo -e "\nLAUNCH TB4_VIZ VIEW_NAVIGATION"
 echo -e"ros2 launch turtlebot4_viz view_navigation.launch.py"
